0ce0e92a2ea74fa0d9aa4738e967a59710767374
hex4coder <the.programmer.luyo@gmail.com>
2025-03-03 07:54:28 +0800

    fix bugs in dap

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,32 +1,36 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    null_ls.setup({
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		null_ls.setup({
 
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-              -- vim.lsp.buf.formatting_sync()
-              vim.lsp.buf.format({ async = false })
-            end,
-          })
-        end
-      end,
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					if bufnr == nil then
+						return
+					end
 
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.completion.lua_ls,
-        --     -- TODO: // masih salah dalam completion code
-        -- null_ls.builtins.completion.ast_grep,
-      },
-    })
-  end,
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+							-- vim.lsp.buf.formatting_sync()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
+
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.completion.lua_ls,
+				--     -- TODO: // masih salah dalam completion code
+				-- null_ls.builtins.completion.ast_grep,
+			},
+		})
+	end,
 }
