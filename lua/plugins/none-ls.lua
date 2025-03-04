00ad4b4f763045ebdfa0f7efae5b7214724e1641
hex4coder <the.programmer.luyo@gmail.com>
2025-03-04 14:38:27 +0800

    update rtee object

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,25 +5,7 @@ return {
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		null_ls.setup({
 
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					if bufnr == nil then
-						return
-					end
-
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-							-- vim.lsp.buf.formatting_sync()
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
+			on_attach = function(client, bufnr) end,
 
 			sources = {
 				null_ls.builtins.formatting.stylua,
